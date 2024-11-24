1faab91264520ed41357c194b4fcb09b1612fb73
Piotr Kowalski <piecioshka@gmail.com>
2024-11-25 00:49:06 +0100

    Change format: use \n for breaking lines

--- src/getSimilarityIndexForText.spec.ts
+++ src/getSimilarityIndexForText.spec.ts
@@ -5,38 +5,22 @@ suite("getSimilarityIndexForText", (test) => {
   test.each([
     {
       text1: `e`,
-      text2: `d
-e`,
+      text2: `d\ne`,
       expected: 33.33,
     },
     {
-      text1: `d
-e`,
-      text2: `c
-d
-e`,
+      text1: `d\ne`,
+      text2: `c\nd\ne`,
       expected: 60,
     },
     {
-      text1: `c
-d
-e`,
-      text2: `b
-c
-d
-e`,
+      text1: `c\nd\ne`,
+      text2: `b\nc\nd\ne`,
       expected: 71.43,
     },
     {
-      text1: `b
-c
-d
-e`,
-      text2: `a
-b
-c
-d
-e`,
+      text1: `b\nc\nd\ne`,
+      text2: `a\nb\nc\nd\ne`,
       expected: 77.78,
     },
   ])("should return $expected", ({ text1, text2, expected }) => {
