7accea66f6c41ad3b40135fda3e19d581aa6cc5b
Piotr Kowalski <piecioshka@gmail.com>
2024-11-25 00:50:15 +0100

    Add UT for smaller second text

--- src/getSimilarityIndexForText.spec.ts
+++ src/getSimilarityIndexForText.spec.ts
@@ -4,24 +4,29 @@ import { getSimilarityIndexForText } from "./getSimilarityIndexForText";
 suite("getSimilarityIndexForText", (test) => {
   test.each([
     {
-      text1: `e`,
-      text2: `d\ne`,
+      text1: "e",
+      text2: "d\ne",
       expected: 33.33,
     },
     {
-      text1: `d\ne`,
-      text2: `c\nd\ne`,
+      text1: "d\ne",
+      text2: "c\nd\ne",
       expected: 60,
     },
     {
-      text1: `c\nd\ne`,
-      text2: `b\nc\nd\ne`,
+      text1: "c\nd\ne",
+      text2: "b\nc\nd\ne",
       expected: 71.43,
     },
     {
-      text1: `b\nc\nd\ne`,
-      text2: `a\nb\nc\nd\ne`,
+      text1: "b\nc\nd\ne",
+      text2: "a\nb\nc\nd\ne",
       expected: 77.78,
+    },
+    {
+      text1: "a\nb\nc\nd\ne",
+      text2: "b\ne",
+      expected: 100,
     },
   ])("should return $expected", ({ text1, text2, expected }) => {
     const result = getSimilarityIndexForText(text1, text2);
